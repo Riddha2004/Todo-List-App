a6729ea11dc9a78ab1d4beb63f0d6ea4c8ff8477
Riddha2004 <riddharc2004@gmail.com>
2024-06-17 22:54:53 +0530

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
   <>
    <header>
-     <h1 className="font-bold text-center mt-8 text-3xl">ToDo List</h1>
+     <h1 className="font-bold text-center mt-8 text-3xl">Todo List</h1>
    </header>
    <main className="p-8">
        <Form/>
